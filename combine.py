c6d852eb9d0b2b8e4f7f5dfda0a58b80f23429c4
Ornyx040 <8405771+Ornyx040@users.noreply.github.com>
2025-09-10 11:24:54 +0900

    Update combine.py

--- combine.py
+++ combine.py
@@ -19,9 +19,9 @@ all_data_sorted = sorted(all_data, key=lambda x: len(x.keys()), reverse=True)
 
 today = datetime.datetime.now().strftime('%Y%m%d')
 
-output_file_dated = f'all_cve_{today}.json'
-with open(output_file_dated, 'w', encoding='utf-8') as out_dated:
-    json.dump(all_data_sorted, out_dated, ensure_ascii=False, separators=(',', ':'))
+#output_file_dated = f'all_cve_{today}.json'
+#with open(output_file_dated, 'w', encoding='utf-8') as out_dated:
+#    json.dump(all_data_sorted, out_dated, ensure_ascii=False, separators=(',', ':'))
 
 output_file_all = 'vulnrichment_all.json'
 with open(output_file_all, 'w', encoding='utf-8') as out_all:
